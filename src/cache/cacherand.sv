6356c7a551614908c2c79b3d2b7486af086b5426
Nathan Bremer <nbremer@rivendell.ecen.okstate.edu>
2024-05-03 18:14:40 -0500

    funtimes

--- src/cache/cacherand.sv
+++ src/cache/cacherand.sv
@@ -72,77 +72,6 @@ module cacherand
   //multiplexer selector to choose between either picking the first empty way, or picking a random way to evict in the LFSR random replacement policy,
   //set to 1 if all of the ways are valid, (meaning that they are all full)
   assign AllValid = &ValidWay; 
-
-  ///// Update replacement bits.
-  // coverage off
-  // Excluded from coverage b/c it is untestable without varying NUMWAYS.
-  function integer log2 (integer value);
-    int val;
-    val = value;
-    for (log2 = 0; val > 0; log2 = log2+1)
-      val = val >> 1;
-    return log2;
-  endfunction // log2
-  // coverage on
-
-  // On a miss we need to ignore HitWay and derive the new replacement bits with the VictimWay.
-  //logic to select between a hit or a miss way
-  //set valid is high when it is inputting a new value into the cache and thus it must use the VictimWayEnc (evicting a value or putting it into a non valid way in the set)
-  mux2 #(LOGNUMWAYS) WayMuxEnc(HitWayEncoded, VictimWayEnc, SetValid, Way);
-
-  /*
-  // bit duplication
-  // expand HitWay as HitWay[3], {{2}{HitWay[2]}}, {{4}{HitWay[1]}, {{8{HitWay[0]}}, ...
-  for(row = 0; row < LOGNUMWAYS; row++) begin
-    localparam integer DuplicationFactor = 2**(LOGNUMWAYS-row-1);
-    localparam StartIndex = NUMWAYS-2 - DuplicationFactor + 1;
-    localparam EndIndex = NUMWAYS-2 - 2 * DuplicationFactor + 2;
-    assign WayExpanded[StartIndex : EndIndex] = {{DuplicationFactor}{Way[row]}};
-  end
-  */
-
-  /*
-  genvar               node;
-  assign LRUUpdate[NUMWAYS-2] = '1;
-  for(node = NUMWAYS-2; node >= NUMWAYS/2; node--) begin : enables
-    localparam ctr = NUMWAYS - node - 1;
-    localparam ctr_depth = log2(ctr);
-    localparam lchild = node - ctr;
-    localparam rchild = lchild - 1;
-    localparam r = LOGNUMWAYS - ctr_depth;
-
-    // the child node will be updated if its parent was updated and
-    // the Way bit was the correct value.
-    // The if statement is only there for coverage since LRUUpdate[root] is always 1. LRU POLICY CAN REMOVE
-
-    if (node == NUMWAYS-2) begin
-      assign LRUUpdate[lchild] = ~Way[r];
-      assign LRUUpdate[rchild] = Way[r];
-    end else begin
-      assign LRUUpdate[lchild] = LRUUpdate[node] & ~Way[r];
-      assign LRUUpdate[rchild] = LRUUpdate[node] & Way[r];
-    end
-  end
-  */
-
-  // The root node of the LRU tree will always be selected in LRUUpdate. No mux needed. LRU POLICY CAN REMOVE
-  //assign NextLRU[NUMWAYS-2] = ~WayExpanded[NUMWAYS-2];
-  //if (NUMWAYS > 2) mux2 #(1) LRUMuxes[NUMWAYS-3:0](CurrLRU[NUMWAYS-3:0], ~WayExpanded[NUMWAYS-3:0], LRUUpdate[NUMWAYS-3:0], NextLRU[NUMWAYS-3:0]);
-
-  // Compute next victim way. LRU POLICY CAN REMOVE Intermediate is the place that you need to input your LFSR vvv !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  /*
-  for(node = NUMWAYS-2; node >= NUMWAYS/2; node--) begin
-    localparam t0 = 2*node - NUMWAYS;
-    localparam t1 = t0 + 1;
-    assign Intermediate[node] = CurrLRU[node] ? Intermediate[t0] : Intermediate[t1];
-  end
-  for(node = NUMWAYS/2-1; node >= 0; node--) begin
-    localparam int0 = (NUMWAYS/2-1-node)*2;
-    localparam int1 = int0 + 1;
-    assign Intermediate[node] = CurrLRU[node] ? int1[LOGNUMWAYS-1:0] : int0[LOGNUMWAYS-1:0];
-  end
-  */
-  
   
 
     //localparam LOG2_NUMWAYS = $clog2(NUMWAYS);
@@ -153,6 +82,7 @@ module cacherand
     logic [WIDTH-1:0] currRandom;
     logic next;
     //logic en; // Ensure en signal is defined
+   logic [WIDTH-1:0] val;
     assign val[0] = 1'b1;
     assign val[WIDTH-1:1] = '0;
     //assign en = 1'b1;
@@ -160,11 +90,11 @@ module cacherand
     priorityonehot #(NUMWAYS) FirstZeroEncoder(~ValidWay, FirstZero);
     binencoder #(NUMWAYS) FirstZeroWayEncoder(FirstZero, FirstZeroWay);
     //mux2 #(LOGNUMWAYS) VictimMux(FirstZeroWay, Intermediate[NUMWAYS-2], AllValid, VictimWayEnc); //THIS IS THE VICTIM MUX that selects between the victim in the case that all ways are valid, and the first zero way in the other case. It outputs VictimWayEnc
-    mux2 #(LOGNUMWAYS) VictimMux(FirstZeroWay, currRandom[WIDTH-1:0], AllValid, VictimWayEnc); //THIS IS THE VICTIM MUX that selects between the victim in the case that all ways are valid, and the first zero way in the other case. It outputs VictimWayEnc
+    mux2 #(LOGNUMWAYS) VictimMux(FirstZeroWay, currRandom[LOGNUMWAYS-1:0], AllValid, VictimWayEnc); //THIS IS THE VICTIM MUX that selects between the victim in the case that all ways are valid, and the first zero way in the other case. It outputs VictimWayEnc
     decoder #(LOGNUMWAYS) decoder (VictimWayEnc, VictimWay);
 
     // LFSR polynomial logic
-    flopenl #(WIDTH) LFSRReg(.clk(clock), .load(reset), .en(LRUWriteEn), .d({next, currRandom[WIDTH-1:1]}), .val, .q(currRandom));
+    flopenl #(WIDTH) LFSRReg(.clk(clk), .load(reset), .en(LRUWriteEn), .d({next, currRandom[WIDTH-1:1]}), .val, .q(currRandom));
 
     if (WIDTH == 3) //two way, degree 2
         assign next = currRandom[2] ^ currRandom[1] ^ currRandom[0];
@@ -185,18 +115,5 @@ module cacherand
   // This is a two port memory.
   // Every cycle must read from CacheSetData and each load/store must write the new LRU.
 
-  // note: Verilator lint doesn't like <= for array initialization (https://verilator.org/warn/BLKLOOPINIT?v=5.021)
-  // Move to = to keep Verilator happy and simulator running fast
-  always_ff @(posedge clk) begin
-    if (reset | (InvalidateCache & ~FlushStage)) 
-      for (int set = 0; set < NUMLINES; set++) LRUMemory[set] = 0; // exclusion-tag: initialize
-    else if(CacheEn) begin //if the cache is enabled
-      // Because we are using blocking assignments, change to LRUMemory must occur after LRUMemory is used so we get the proper value
-      //checks if the write is allowed to the flip flops that store the bits that tell you what way to 
-      if(LRUWriteEn & (PAdr == CacheSetTag)) CurrLRU = NextLRU;
-      else                                   CurrLRU = LRUMemory[CacheSetTag];
-      if(LRUWriteEn)                         LRUMemory[PAdr] = NextLRU;
-    end
-  end
 
 endmodule
