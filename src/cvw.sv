7ee498c28321adb2485d321eac97e347ec7b3b36
Nate Bremer <109544222+SergeantRipJaw@users.noreply.github.com>
2024-05-06 18:16:34 -0500

    Update cvw.sv
    updaded cvw with new parameters

--- src/cvw.sv
+++ src/cvw.sv
@@ -72,6 +72,9 @@ typedef struct packed {
   logic         DCACHE_SUPPORTED;
   logic         ICACHE_SUPPORTED;
 
+  int           D_CACHE_REPLACEMENT
+  int           I_CACHE_REPLACEMENT
+
 // TLB configuration.  Entries should be a power of 2
   int           ITLB_ENTRIES;
   int           DTLB_ENTRIES;
